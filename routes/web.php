beb57134e47fb25536988576f1e16c304fa8e8f8
Rustem-A <faustyanishe@gmail.com>
2020-04-19 14:01:49 +0300

    debugbar init

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Barryvdh\Debugbar\Facade as Debugbar;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    Debugbar::error('Error!');
     return view('welcome');
 });
