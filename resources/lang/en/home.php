f718a7d232af111918136fbc3656baf18c5a568f
Rustem-A <faustyanishe@gmail.com>
2020-05-01 11:25:30 +0300

    home view update

---
+++ resources/lang/en/home.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    "title" => "Welcome to Task Manager project!",
+    "about_project" => "The project Task Manager was created as a training project on 
+        <a href='https://laravel.com/'>Laravel</a> framework.",
+    "p1" => "Task Manager - project to manage and share tasks.",
+    "p2" => "Task Manager will help you to create new tasks, 
+        assign executors, to take already created tasks for execution.",
+    "p3" => "The list of current tasks is available on the corresponding <a href='/tasks'>page</a>, 
+        as well as the list of <a href='/users'>users</a> participating in the project. 
+        Links to these pages are available in the navigation bar at the top of each page.",
+    "p4" => "In the base state, tasks have four statuses - 'new task', 
+        'working', 'testing', 'terminated'. If desired, you can add new statuses or rename existing ones.
+        On the tasks page you can also correspond with other users.",
+    "p5" => "Task Manager currently supports two interface languages: 
+        <a href='/lang/ru'>Russian</a> and <a href='/lang/en'>English</a>. 
+        You can always switch between languages using the navigation bar.",
+    "p6" => "To participate in the project, please go through <a href='/register'>registration</a> procedure.",
+];
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,19 @@
 @extends('layouts.app')
 
-@section('header')
-Main Page
-@endsection
+@section('header', 'Task Manager')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Welcome!
-                </div>
-            </div>
-        </div>
+    <div class="jumbotron">
+        <h3 class="text-center">{{ __('home.title') }}</h3>
+        <p class="lead text-center">{!! __('home.about_project') !!}</p>
+        <hr class="my-4">
+        <p>{!! __('home.p1') !!}</p>
+        <p>{!! __('home.p2') !!}</p>
+        <p>{!! __('home.p3') !!}</p>
+        <p>{!! __('home.p4') !!}</p>
+        <p>{!! __('home.p5') !!}</p>
+@if(!Auth::check())
+        <p>{!! __('home.p6') !!}</p>
+@endif
     </div>
-</div>
 @endsection
